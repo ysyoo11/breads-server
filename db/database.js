b93a8347eaf5dd2499a8b91ab5331c7753826d00
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-10-31 14:33:03 +1100

    feat: Set up app with database connected

---
+++ db/database.js
@@ -0,0 +1,10 @@
+import { Sequelize } from 'sequelize';
+
+import { config } from '../config.js';
+
+const { host, user, database, password } = config.db;
+export const sequelize = new Sequelize(database, user, password, {
+  host,
+  dialect: 'mysql',
+  logging: false,
+});
